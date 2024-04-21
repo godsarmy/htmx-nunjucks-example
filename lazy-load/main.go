18eae919b2b6eb7d89d4793b0faab01fba763d82
Walt Chen <godsarmycy@gmail.com>
2024-04-21 09:31:57 -0400

    exmaple of lazy load

---
+++ lazy-load/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.Delims("{[{", "}]}")
+	router.LoadHTMLGlob("./templates/*.tmpl")
+
+    router.Static("/img", "./img")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html.tmpl", gin.H{})
+	})
+
+	router.GET("/graph", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"title": "Tokyo Climate", "path": "/img/tokyo.png"})
+	})
+
+	router.Run(":8080")
+}
