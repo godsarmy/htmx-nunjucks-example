b6c849d03cb3f23fb6189244c6fcc5e8bcd97aef
Walt Chen <godsarmycy@gmail.com>
2024-05-03 08:30:26 -0400

    add example of sortable

---
+++ sortable/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Item struct {
+	Name string `json:"name"`
+	ID   int    `json:"id"`
+}
+
+type ItemList struct {
+	Item []string `json:"item"  binding:"required"`
+}
+
+var items []Item
+
+func init_db() {
+
+	items = []Item{
+		Item{
+			Name: "Item 1",
+			ID:   1,
+		},
+		Item{
+			Name: "Item 2",
+			ID:   2,
+		},
+		Item{
+			Name: "Item 3",
+			ID:   3,
+		},
+		Item{
+			Name: "Item 4",
+			ID:   4,
+		},
+		Item{
+			Name: "Item 5",
+			ID:   5,
+		},
+	}
+}
+
+func find_item(id int) *Item {
+	var found *Item
+	for _, item := range items {
+		if item.ID == id {
+			found = &item
+		}
+	}
+	return found
+}
+
+func main() {
+
+	init_db()
+	fmt.Println(items)
+
+	router := gin.Default()
+	router.Delims("{[{", "}]}")
+	router.LoadHTMLGlob("./templates/*.tmpl")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html.tmpl", gin.H{})
+	})
+
+	router.POST("/items", func(c *gin.Context) {
+		var order_list ItemList
+
+		err := json.NewDecoder(c.Request.Body).Decode(&order_list)
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+		}
+
+		if len(order_list.Item) == 0 {
+			c.JSON(http.StatusOK, items)
+			return
+		}
+
+		var sorted []Item
+		for _, id := range order_list.Item {
+			id_num, err := strconv.Atoi(id)
+			if err != nil {
+				c.AbortWithError(http.StatusBadRequest, err)
+				return
+			}
+			item := find_item(id_num)
+
+			if item != nil {
+				sorted = append(sorted, *item)
+			}
+		}
+		c.JSON(http.StatusOK, sorted)
+	})
+
+	router.Run(":8080")
+}
