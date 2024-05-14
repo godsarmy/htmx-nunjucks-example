f6f76190210d093af5a02d8d263f5942399a3435
Walt Chen <godsarmycy@gmail.com>
2024-05-13 21:15:37 -0400

    Add restored extension example

---
+++ restored/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"embed"
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var htmx_version = "latest"
+var nunjucks_version = "3.2.4"
+
+//go:embed templates/*
+var embed_fs embed.FS
+
+func main() {
+
+	router := gin.Default()
+	templ := template.Must(
+		template.New("").Delims("{[{", "}]}").ParseFS(embed_fs, "templates/*.tmpl"),
+	)
+	router.SetHTMLTemplate(templ)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html.tmpl",
+			gin.H{
+				"htmx_version":     htmx_version,
+				"nunjucks_version": nunjucks_version,
+			},
+		)
+	})
+
+	router.GET("/example", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Restored example!"})
+	})
+
+	router.GET("/other_page", func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("other_page"))
+	})
+
+	router.Run(":8080")
+}
