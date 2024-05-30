c32485696ba9ff3b828714441ed9649b954d14ca
Walt Chen <735975+godsarmy@users.noreply.github.com>
2024-05-29 22:42:09 -0400

    Add file-upload example

---
+++ file-upload/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var htmx_version = "latest"
+var nunjucks_version = "3.2.4"
+var bootstrap_version = "latest"
+
+//go:embed templates/*
+var embed_fs embed.FS
+
+func main() {
+
+	router := gin.Default()
+	templ := template.Must(
+		template.New("").Delims("{[{", "}]}").ParseFS(embed_fs, "templates/*.tmpl"),
+	)
+	router.SetHTMLTemplate(templ)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html.tmpl",
+			gin.H{
+				"htmx_version":      htmx_version,
+				"nunjucks_version":  nunjucks_version,
+				"bootstrap_version": bootstrap_version,
+			},
+		)
+	})
+
+	router.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		fmt.Println(file.Filename)
+		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("%s uploaded!", file.Filename)})
+	})
+
+	router.Run(":8080")
+}
