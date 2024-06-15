aafd7aaddc27b31c0b262597a52115e6d43850b3
Walt Chen <735975+godsarmy@users.noreply.github.com>
2024-06-15 09:49:06 -0400

    Add example of server-sent-events

---
+++ server-sent-events/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+    "io"
+	"net/http"
+    "time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var htmx_version = "latest"
+var nunjucks_version = "3.2.4"
+var bootstrap_version = "latest"
+
+//go:embed templates/*
+var embed_fs embed.FS
+var message chan string
+
+func main() {
+    message = make(chan string)
+
+    go func() {
+		for {
+			time.Sleep(time.Second * 10)
+			now := time.Now().Format("2006-01-02 15:04:05")
+			currentTime := fmt.Sprintf("The Current Time Is %v", now)
+
+			// Send current time to clients message channel
+			message <- currentTime
+		}
+	}()
+
+	router := gin.Default()
+	templ := template.Must(
+		template.New("").Delims("{[{", "}]}").ParseFS(embed_fs, "templates/*.tmpl"),
+	)
+	router.SetHTMLTemplate(templ)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html.tmpl",
+			gin.H{
+				"htmx_version":      htmx_version,
+				"nunjucks_version":  nunjucks_version,
+				"bootstrap_version": bootstrap_version,
+			},
+		)
+	})
+
+	router.GET("/message", func(c *gin.Context) {
+        c.Stream(func(w io.Writer) bool {
+			// Stream message to client from message channel
+			if msg, ok := <-message; ok {
+				c.SSEvent("message", msg)
+				return true
+			}
+			return false
+		})
+	})
+	router.Run(":8080")
+}
